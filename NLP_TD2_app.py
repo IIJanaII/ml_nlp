2fdabfb93f37e138ce308ecf9c0824fad2c8be62
Janany J <104461084+IIJanaII@users.noreply.github.com>
2023-10-12 16:48:51 +0200

    Update NLP_TD2_app.py

--- NLP_TD2_app.py
+++ NLP_TD2_app.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize
 
 # Load NLTK resources
 nltk.download('sentiwordnet')
+nltk.download('punkt')
 
 # Define functions to calculate sentiment scores
 def get_adverbs(text):
@@ -22,7 +23,7 @@ def calculate_sentiment_score(adverbs):
     return sentiment_score
 
 # Streamlit UI
-st.title("Movie Review Sentiment Analysis")
+st.title("Janany's Movie Review Sentiment Analysis")
 
 # User input
 review = st.text_area("Enter your movie review:")
